571e86d6847b742fadcaca80efde1b9014e3a4dd
Jason Rosenman <jvrosenman@gmail.com>
2017-12-31 13:41:18 -0500

    Added basic callback to Leader class

--- leader.py
+++ leader.py
@@ -1,10 +1,14 @@
 import network
 import socket
+import uosc.server
 
 class Leader:
     screen = None
     def __init__(self, scr):
         self.screen = scr
+
+    def osc_callback(self, time, msg):
+        print("Got OSC message " + str(msg))
 
     def start(self, ssid):
         ap_if = network.WLAN(network.AP_IF)
@@ -15,3 +19,4 @@ class Leader:
         addr = '10.11.10.1'
         ap_if.ifconfig((addr, '255.255.255.0', '10.11.10.1', '10.11.10.1'))
         self.screen.print("IP: " + addr)
+        uosc.server.run_server(addr, 9000, callback=self.osc_callback)
